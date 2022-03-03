7d91735843d5fde08969f70e8097485c2c9aa93c
Pulkit Mittal <mittal.pulkit@technonet.co.jp>
2022-03-03 18:00:25 +0900

    add more funds

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -4,4 +4,34 @@ import { MF } from '../interfaces';
 export const mfList: MF[] = [
   { id: 120465, name: 'Axis Bluechip Fund - Direct Plan - Growth' },
   { id: 120468, name: 'Axis Focused 25 Fund - Direct Plan - Growth Option' },
+  { id: 120473, name: 'Axis Gold Fund - Direct Plan - Growth option' },
+  { id: 145110, name: 'Axis Growth Opportunities Fund - Direct Plan - Growth' },
+  { id: 125354, name: 'Axis Small Cap Fund - Direct Plan - Growth' },
+  {
+    id: 120594,
+    name: 'ICICI Prudential Technology Fund - Direct Plan -  Growth',
+  },
+  {
+    id: 118834,
+    name: 'Mirae Asset Emerging Bluechip Fund - Direct Plan - Growth',
+  },
+  { id: 143783, name: 'Mirae Asset Healthcare Fund Direct Growth' },
+  {
+    id: 129046,
+    name: 'Motilal Oswal Flexi cap Fund Direct Plan-Growth Option',
+  },
+  {
+    id: 145552,
+    name: 'Motilal Oswal Nasdaq 100 Fund of Fund- Direct Plan Growth',
+  },
+  {
+    id: 118778,
+    name: 'Nippon India Small Cap Fund - Direct Plan Growth Plan - Growth Option',
+  },
+  { id: 125497, name: 'SBI Small Cap Fund - Direct Plan - Growth' },
+  { id: 135800, name: 'Tata Digital India Fund-Direct Plan-Growth' },
+  {
+    id: 135810,
+    name: 'Tata India Pharma & Healthcare Fund-Direct Plan-Growth',
+  },
 ];
